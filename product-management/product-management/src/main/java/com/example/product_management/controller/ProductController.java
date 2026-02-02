fa7709c2ff6fc9c66f7348d476f467db0bd4000d
Chè <119411856+1tra2016@users.noreply.github.com>
2026-02-02 17:51:53 +0700

    Add files via upload

---
+++ product-management/product-management/src/main/java/com/example/product_management/controller/ProductController.java
@@ -0,0 +1,21 @@
+package com.example.product_management.controller;
+
+import com.example.product_management.model.Product;
+import com.example.product_management.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/api/products")
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+}
---
+++ product-management/product-management/src/main/java/com/example/product_management/service/ProductService.java
@@ -0,0 +1,23 @@
+package com.example.product_management.service;
+
+import com.example.product_management.model.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private List<Product> products = new ArrayList<>();
+
+    public ProductService() {
+        products.add(new Product(1, "Laptop", 1500));
+        products.add(new Product(2, "Chuột", 20));
+        products.add(new Product(3, "Bàn phím", 45));
+    }
+
+    public List<Product> getAllProducts() {
+        return products;
+    }
+}
